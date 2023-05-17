3585289b5c65ace256e519b1d7c6d132db0891ec
Jonas Silva <jonas86@edu.unisinos.br>
2023-05-17 20:27:44 -0300

    primeiro usuario

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("ola mundo");
+import express, { Response, Request } from "express";
+import { Growdever } from "./models/growdever";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  console.log("entrou na rota");
+  const growdever1 = new Growdever("1", "jonas", 27);
+  res.status(200).send({ ok: "true", data: growdever1.toJson() });
+});
+
+app.listen(3333, () => {
+  console.log("api is running");
+});
